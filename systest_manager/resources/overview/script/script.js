f94b1438f67330713238a89e554bdd8ce098b116
Dan Kilman <dankilman@gmail.com>
2015-07-16 14:31:53 +0300

    cleanup

--- systest_manager/resources/overview/script/script.js
+++ systest_manager/resources/overview/script/script.js
@@ -1,11 +1,7 @@
 function OverviewController($http, $scope, $timeout, $log) {
 
   $http.get('state').then(function(response) {
-    var state = response.data;
-    $scope.configuration = state.configuration;
-    $scope.host = state.host;
-    $scope.version = state.version;
-    $scope.deployments = state.deployments;
+    _.extend($scope, response.data);
   });
 
 }
