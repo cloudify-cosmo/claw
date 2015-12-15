ce2a890a6fd76c068151b49c0dd2367a66a58b41
Dan Kilman <dankilman@gmail.com>
2015-12-15 23:42:28 +0200

    create stubs for a react based overview

--- systest_manager/overview.py
+++ systest_manager/overview.py
@@ -9,54 +9,21 @@ class Overview(object):
 
     def __init__(self, configuration):
         self.app = bottle.app()
-        self.configuration = configuration
+        self.conf = configuration
         self.client = configuration.client
-        self.host = self.client._client.host
-
-    def metadata(self):
-        return {
-            'version': self.client.manager.get_version()['version'],
-            'host': self.host,
-            'configuration': self.configuration.configuration
-        }
-
-    def state(self):
-        deployment_ids = [d.id for d in
-                          self.client.deployments.list(_include=['id'])]
-        deployments = {d_id: {'node_instances': [],
-                              'executions': []}
-                       for d_id in deployment_ids}
-        node_instances = self.client.node_instances.list()
-        executions = self.client.executions.list()
-
-        for node_instance in node_instances:
-            deployment = deployments[node_instance.deployment_id]
-            deployment['node_instances'].append(node_instance)
-        for execution in executions:
-            deployment = deployments[execution.deployment_id]
-            deployment['executions'].append(execution)
-
-        return {'deployments': deployments}
-
-    def events(self, execution_id):
-        node_id = bottle.request.query.get('node_id')
-        events, _ = self.client.events.get(
-            execution_id=execution_id,
-            batch_size=1000,
-            include_logs=True)
-        if node_id:
-            events = [e for e in events
-                      if e['context'].get('node_id') == node_id]
-        for event in events:
-            event['type'] = ('LOG' if 'cloudify_log' in event['type']
-                             else 'CFY')
-
-        return {'events': events}
 
     @staticmethod
     def static(filename):
         return bottle.static_file(filename,
                                   root=os.path.join(resources.DIR, 'overview'))
+
+    def comments(self):
+        return {
+            'comments': [
+                {'id': 1, 'author': "Pete Hunt", 'text': "This is one comment"},
+                {'id': 2, 'author': "Jordan Walk!", 'text': "This is *another* comment"}
+            ]
+        }
 
     def index(self):
         return self.static('index.html')
@@ -64,14 +31,12 @@ class Overview(object):
     def serve(self, port):
         routes = {
             '/': self.index,
-            '/metadata': self.metadata,
-            '/state': self.state,
-            '/events/<execution_id>': self.events,
+            '/comments': self.comments,
             '/static/<filename:path>': self.static,
         }
         for route, handler in routes.items():
             self.app.route(route)(handler)
-        print 'http://localhost:{0}'.format(port)
+        self.conf.logger.info('http://localhost:{0}'.format(port))
         self.app.run(debug=True, port=port)
 
 
--- systest_manager/resources/overview/script/script.js
+++ systest_manager/resources/overview/script/script.js
@@ -1,24 +1,71 @@
-function OverviewController($http, $scope, $timeout, $log) {
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">{this.props.author}</h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
 
-  $scope.fetchEvents = function(nodeInstanceId) {
-    var executionID = '3a4cd9dd-08f3-499a-afb1-7520d4600939';
-    $http.get('/events/'+executionID+'?node_id='+nodeInstanceId).then(
-      function(response) {
-        $scope.events = response.data.events;
-      });
-  };
-
-  $http.get('/metadata').then(function(response) {
-    $scope.metadata = response.data;
-  });
-
-  var loadData = function() {
-    $http.get('/state').then(function(response) {
-      $scope.deployments = response.data.deployments;
-      $timeout(loadData, 5000);
+var CommentList = React.createClass({
+  render: function() {
+    var comments = this.props.data.comments;
+    var commentNodes = comments.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
     });
-  };
+    return (<div className="commentList">{commentNodes}</div>);
+  }
+});
 
-  loadData();
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
 
-}
+var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: {comments: []}};
+  },
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    //setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox url="/comments" pollInterval={2000} />,
+  document.getElementById('content')
+);
