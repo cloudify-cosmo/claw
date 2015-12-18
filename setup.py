fbfd1ce38aa7df0921f244c8309368b9e3995379
Dan Kilman <dankilman@gmail.com>
2015-12-18 21:33:08 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='0.1',
     author='Dan Kilman',
     author_email='dankilman@gmail.com',
-    packages=['systest_manager'],
+    packages=['systest_manager',
+              'systest_manager.handlers',
+              'systest_manager.resources'],
     description='Manages cloudify system tests configurations',
     zip_safe=False,
     install_requires=[
