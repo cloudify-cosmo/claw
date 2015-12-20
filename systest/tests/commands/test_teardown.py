8237a2d2f6d4faac318da19e3b96c037b172e025
Dan Kilman <dankilman@gmail.com>
2015-12-20 12:43:09 +0200

    start creating test stubs

---
+++ systest/tests/commands/test_teardown.py
@@ -0,0 +1,35 @@
+########
+# Copyright (c) 2015 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############
+
+from systest import tests
+
+
+class TeardownTest(tests.BaseTest):
+
+    def test_basic(self):
+        pass
+
+    def test_no_existing_configuration(self):
+        pass
+
+    def test_existing_blueprint_reset(self):
+        pass
+
+    def test_existing_blueprint_no_reset(self):
+        pass
+
+    def test_no_inputs_file(self):
+        pass
