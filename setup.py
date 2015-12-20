82007b764a184e8ea99c9b528f5ffc906fed6e9d
Dan Kilman <dankilman@gmail.com>
2015-12-20 11:33:12 +0200

    add licenses

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
+########
+# Copyright (c) 2015 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############
+
 from setuptools import setup
 
 setup(
     name='systest',
     version='0.1',
-    author='Dan Kilman',
-    author_email='dankilman@gmail.com',
+    author='GigaSpaces',
+    author_email='cosmo-admin@gigaspaces.com',
     packages=['systest',
               'systest.handlers',
               'systest.resources'],
     description='Manages Cloudify manager environments by leveraging the'
                 'system tests framework',
+    license='Apache License, Version 2.0',
     zip_safe=False,
     install_requires=[
         'cloudify-system-tests',
