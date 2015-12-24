fca9d510c0131aca7f5acf04715f51160cb28e54
Dan Kilman <dankilman@gmail.com>
2015-12-24 09:31:00 +0200

    add status tests

--- systest/tests/commands/test_status.py
+++ systest/tests/commands/test_status.py
@@ -13,20 +13,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ############
+import sh
 
+from systest import configuration
 from systest import tests
 
 
 class StatusTest(tests.BaseTest):
 
+    def setUp(self):
+        super(StatusTest, self).setUp()
+        self.init()
+
     def test_basic(self):
+        # TODO
         pass
 
     def test_no_configuration(self):
-        pass
+        with self.assertRaises(sh.ErrorReturnCode) as c:
+            self.systest.status(tests.STUB_CONFIGURATION)
+        self.assertIn('Not initialized', c.exception.stderr)
 
     def test_no_manager_ip(self):
-        pass
+        self.systest.generate(tests.STUB_CONFIGURATION)
+        with self.assertRaises(sh.ErrorReturnCode) as c:
+            self.systest.status(tests.STUB_CONFIGURATION)
+        self.assertIn('Not bootstrapped', c.exception.stderr)
 
     def test_no_connectivity_to_manager(self):
-        pass
+        self.systest.generate(tests.STUB_CONFIGURATION)
+        conf = configuration.Configuration(tests.STUB_CONFIGURATION)
+        ip = '127.0.0.1'
+        with conf.patch.handler_configuration as patch:
+            patch.set_value('manager_ip', ip)
+        with self.assertRaises(sh.ErrorReturnCode) as c:
+            self.systest.status(tests.STUB_CONFIGURATION)
+        self.assertIn('Not reachable', c.exception.stderr)
+        self.assertIn(ip, c.exception.stderr)
